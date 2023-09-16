e5a1d0f1c40b46ecc1cfe3495b5b292db20aaa42
char1ey <ckstn410@gmail.com>
2023-09-16 16:31:24 +0900

    Feat: 실행컨텍스트 끝

---
+++ 230916/context2.js
@@ -0,0 +1,34 @@
+// let a = 'first'
+
+// function b(arguments) {
+//     console.log(arguments + a)
+// }
+
+// function c() {
+//     let a = 'second'
+//     console.log(a)
+//     b('a = ')
+// }
+
+// c()
+
+a = 1;
+
+var b = 2;
+
+cFunc(10)
+
+cFunc = function (e) {
+    var c = 10;
+    var d = 15;
+
+    a = 3
+
+    function dFunc() {
+        var f = 5;
+    }
+
+    dFunc();
+}
+
+cFunc(10);
